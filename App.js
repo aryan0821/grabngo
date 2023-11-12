a1e283db8f1d03a36d347c02689f01a6fe9e020c
Aryan Nair <nairaryan21@gmail.com>
2023-11-11 19:02:18 -0500

    questions-ui

--- App.js
+++ App.js
@@ -3,9 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen'; // Make sure to create this screen
 import HomeScreen from './screens/HomeScreen'; // Import HomeScreen
-import OrderOptionsScreen from './screens/OrderOptionsScreen'; // Import OrderOptionsScreen
-import MenuScreen from './screens/MenuScreen';
-import ConfirmOrderScreen from './screens/ConfirmOrderScreen';
+import QuestionScreen from './screens/QuestionScreen';
 
 // Create a stack navigator
 const Stack = createNativeStackNavigator();
@@ -16,8 +14,7 @@ function App() {
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="MenuScreen" component={MenuScreen} />
-        <Stack.Screen name="ConfirmOrderScreen" component={ConfirmOrderScreen} />
+        <Stack.Screen name="Question" component={QuestionScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
